acf42d33785936061728777ccfbed947f4a0b644
Ujjaval-Karangiya <ujjavalk2401@gmail.com>
2025-09-04 01:36:44 +0530

    final show 2

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,12 +2,14 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import authRoutes from "./routes/auth.routes.js";
+// import authRoutes from "./routes/auth.route.js";
+// app.use("/auth", authRoutes);
 
 import path from "path";
 
 import { connectDB } from "./lib/db.js";
 
-import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
